fd363dd0f36a21b30eda073578f3aa87f847b6fc
bhaskarreddy94 <bhaskar.boss906@gmail.com>
2019-08-27 08:56:30 +0530

    add setters and getters for pageModel

--- ATOS/src/main/java/com/ats/ATOS/PageModel.java
+++ ATOS/src/main/java/com/ats/ATOS/PageModel.java
@@ -2,11 +2,33 @@ package com.ats.ATOS;
 
 import lombok.Data;
 
-@Data
+//@Data
+/**
+ * @author SUNDAR
+ *
+ */
 public class PageModel {
 	private int page;
 	private int size;
 	private String properties;
+	public int getPage() {
+		return page;
+	}
+	public void setPage(int page) {
+		this.page = page;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public String getProperties() {
+		return properties;
+	}
+	public void setProperties(String properties) {
+		this.properties = properties;
+	}
 	
 
 }
